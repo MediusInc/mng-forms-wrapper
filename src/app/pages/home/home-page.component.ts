e43e75d580cbe7ac9b925cb710afb429c3d61ca5
Luka Šarc <luka.sarc@medius.si>
2023-01-04 11:15:36 +0100

    fix for init vaadin app on every home page init

--- src/app/pages/home/home-page.component.ts
+++ src/app/pages/home/home-page.component.ts
@@ -1,7 +1,37 @@
-import {Component} from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
 
 @Component({
     selector: 'app-home-page',
     templateUrl: './home-page.component.html'
 })
-export class HomePageComponent {}
+export class HomePageComponent implements AfterViewInit {
+    ngAfterViewInit(): void {
+        const vaadin = window['vaadin' as keyof object] as any;
+        vaadin.initApplication("forms", {
+            "browserDetailsUrl": "forms/app?embedded=true",
+            "serviceUrl": "/forms/app",
+            "widgetset": "com.vaadin.DefaultWidgetSet",
+            "theme": "valo-littea-tip",
+            "versionInfo": {"vaadinVersion": "7.7.17"},
+            "vaadinDir": "forms/VAADIN/",
+            "heartbeatInterval": 15,
+            "debug": false,
+            "standalone": false,
+            "authErrMsg": {
+                "message": "Zapišite si vse neshranjene podatke " +
+                    "za nadaljevanje <u>kliknite tukaj<\/u>.",
+                "caption": "Napaka pri avtentikaciji"
+            },
+            "comErrMsg": {
+                "message": "Zapišite si vse neshranjene podatke " +
+                    "za nadaljevanje <u>kliknite tukaj<\/u>.",
+                "caption": "Napaka pri povezavi"
+            },
+            "sessExpMsg": {
+                "message": "Zapišite si vse neshranjene podatke " +
+                    "za nadaljevanje <u>kliknite tukaj<\/u>.",
+                "caption": "Seja je potekla"
+            }
+        });
+    }
+}
--- src/assets/vaadinBootstrap.js
+++ src/assets/vaadinBootstrap.js
@@ -359,37 +359,6 @@
 		}
 	};
 
-	log('Vaadin bootstrap loaded');
+	console.log('Vaadin bootstrap loaded');
 })();
-
-
-if (window.vaadin)
-{
-    vaadin.initApplication("forms", {
-        "browserDetailsUrl": "forms/app?embedded=true",
-        "serviceUrl": "/forms/app",
-        "widgetset": "com.vaadin.DefaultWidgetSet",
-        "theme": "valo-littea-tip",
-        "versionInfo": {"vaadinVersion": "7.7.17"},
-        "vaadinDir": "forms/VAADIN/",
-        "heartbeatInterval": 15,
-        "debug": false,
-        "standalone": false,
-        "authErrMsg": {
-            "message": "Zapišite si vse neshranjene podatke " +
-                "za nadaljevanje <u>kliknite tukaj<\/u>.",
-            "caption": "Napaka pri avtentikaciji"
-        },
-        "comErrMsg": {
-            "message": "Zapišite si vse neshranjene podatke " +
-                "za nadaljevanje <u>kliknite tukaj<\/u>.",
-            "caption": "Napaka pri povezavi"
-        },
-        "sessExpMsg": {
-            "message": "Zapišite si vse neshranjene podatke " +
-                "za nadaljevanje <u>kliknite tukaj<\/u>.",
-            "caption": "Seja je potekla"
-        }
-    });
-}
 //]] >
